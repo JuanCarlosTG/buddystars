8c04275a4515ef3dacac3debcfa5b6132cc026b1
RubensHdz <ruben@kreativeco.com>
2016-04-11 00:40:51 -0500

    nueva barra de status

--- app/src/main/java/mx/com/ioblok/buddystars/home/fragments/PointsFragment.java
+++ app/src/main/java/mx/com/ioblok/buddystars/home/fragments/PointsFragment.java
@@ -4,13 +4,16 @@ import android.animation.ObjectAnimator;
 import android.app.Fragment;
 import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.DecelerateInterpolator;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -29,8 +32,13 @@ public class PointsFragment extends Fragment implements WebBridge.WebBridgeListe
     View v;
     TextView tv_total_pts, tv_portability, tv_new;
     ProgressBar pBarNew, pBarPor;
+    int progressValue = 0;
+    Button stopProgressBtn;
+
     int pStatus = 0;
     private Handler handler = new Handler();
+
+    ProgressTask task = new ProgressTask();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -45,6 +53,10 @@ public class PointsFragment extends Fragment implements WebBridge.WebBridgeListe
         pBarNew = (ProgressBar) v.findViewById(R.id.progress_new);
         pBarPor = (ProgressBar) v.findViewById(R.id.progress_portability);
 
+        stopProgressBtn = (Button) v.findViewById(R.id.btnCancel);
+
+        showProgress();
+        /*
         new Thread(new Runnable() {
 
             @Override
@@ -71,12 +83,14 @@ public class PointsFragment extends Fragment implements WebBridge.WebBridgeListe
                     }
                 }
             }
-        }).start();
+        }).start();*/
 
         getData();
         return v;
 
     }
+
+
 
     public void getData() {
         WebBridge.send("/report-alpha", "Enviando", getActivity(), this);
@@ -112,6 +126,67 @@ public class PointsFragment extends Fragment implements WebBridge.WebBridgeListe
     @Override
     public void onWebBridgeFailure(String url, String response) {
         Log.e("Mal", response);
+    }
+
+    private class ProgressTask extends AsyncTask<Integer,Integer,Void>{
+
+        protected void onPreExecute() {
+            super.onPreExecute(); ///////???????
+            pBarNew.setMax(100); // set maximum progress to 100.
+            Log.e("entra aqui" , String.valueOf(progressValue));
+
+        }
+        protected void onCancelled() {
+            pBarNew.setMax(40); // stop the progress
+            Log.v("Progress cancel", String.valueOf(progressValue));
+        }
+
+        protected Void doInBackground(Integer... params) {
+            int start=params[0];
+            for(int i=start;i<=100;i+=1){
+                try {
+                    boolean cancelled=isCancelled();
+                    if (i == 40){
+                        stopProgress();
+                        Log.e("detente", String.valueOf(i));
+                    }else {
+                        if (!cancelled) {
+                            Log.e("progreso", String.valueOf(i));
+                            ///////////////////////////publishProgress(i); ///
+                            pBarNew.setProgress(i);
+                            Log.v("Progress increment", String.valueOf(i));
+                            ///////////////////////////onProgressUpdate(i); ///
+                            SystemClock.sleep(500);
+                        }
+                    }
+                } catch (Exception e) {
+                    Log.e("Error", e.toString());
+                }
+            }
+            return null;
+        }
+        protected void onProgressUpdate(Integer... values) {
+
+            // increment progress bar by progress value
+            //////////////////////setProgress(10);
+            //////////////////////prgs.setProgress(prgs.getProgress() + 5); // the bar does not fill 100%
+            pBarNew.setProgress(1);
+            Log.v("Progress","Once");
+        }
+        protected void onPostExecute(Void result) {
+            // async task finished
+            Log.v("Progress", "Finished");
+        }
+
+    }
+
+    public void showProgress() {
+        task.execute(1);
+    }
+
+    public void stopProgress() {
+        task.cancel(true);
+        Log.e("detente", String.valueOf(progressValue));
     }
 
 }
