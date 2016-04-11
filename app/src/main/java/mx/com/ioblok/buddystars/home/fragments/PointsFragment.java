55ee781bca7c58cc2552c2811a61ddf2f47ca76d
RubensHdz <ruben@kreativeco.com>
2016-04-10 22:17:25 -0500

    barras de progreso en puntos

--- app/src/main/java/mx/com/ioblok/buddystars/home/fragments/PointsFragment.java
+++ app/src/main/java/mx/com/ioblok/buddystars/home/fragments/PointsFragment.java
@@ -1,13 +1,19 @@
 package mx.com.ioblok.buddystars.home.fragments;
 
+import android.animation.ObjectAnimator;
 import android.app.Fragment;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
@@ -22,6 +28,10 @@ public class PointsFragment extends Fragment implements WebBridge.WebBridgeListe
 
     View v;
     TextView tv_total_pts, tv_portability, tv_new;
+    ProgressBar pBarNew, pBarPor;
+    int pStatus = 0;
+    private Handler handler = new Handler();
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -32,12 +42,43 @@ public class PointsFragment extends Fragment implements WebBridge.WebBridgeListe
         tv_new = (TextView) v.findViewById(R.id.tv_new);
         tv_portability = (TextView) v.findViewById(R.id.tv_portability);
 
+        pBarNew = (ProgressBar) v.findViewById(R.id.progress_new);
+        pBarPor = (ProgressBar) v.findViewById(R.id.progress_portability);
+
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                while (pStatus < 100) {
+                    pStatus += 1;
+
+                    handler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            pBarNew.setProgress(pStatus);
+                            pBarPor.setProgress(pStatus);
+                        }
+                    });
+                    try {
+                        // Sleep for 200 milliseconds.
+                        // Just to display the progress slowly
+                        Thread.sleep(50);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+
         getData();
         return v;
 
     }
 
-    public void getData(){
+    public void getData() {
         WebBridge.send("/report-alpha", "Enviando", getActivity(), this);
     }
 
@@ -48,7 +89,7 @@ public class PointsFragment extends Fragment implements WebBridge.WebBridgeListe
 
         try {
 
-            JSONObject main  = json.getJSONObject("data");
+            JSONObject main = json.getJSONObject("data");
             String tp = main.getString("total_points");
             String tn = main.getString("total_new");
             String tpb = main.getString("total_portability");
@@ -70,7 +111,7 @@ public class PointsFragment extends Fragment implements WebBridge.WebBridgeListe
 
     @Override
     public void onWebBridgeFailure(String url, String response) {
-        Log.e("Mal" ,response);
+        Log.e("Mal", response);
     }
 
 }
